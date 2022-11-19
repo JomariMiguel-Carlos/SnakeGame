92d6f339a955cad1a711c28c2284ce85d9dd552f
[JomariMiguel-Carlos] <[jomarimadla0@gmail.com]>
2022-11-19 16:38:03 +0800

    how the game works

--- mysnakegame.py
+++ mysnakegame.py
@@ -45,5 +45,42 @@ pen.goto(0, 250)
 pen.write("Score: 0    High Score: 0", align="center",
           font=("Times New Roman", 24, "bold"))
 
+#assigning key for movements
 
+def group():
+    if head.direction != "down":
+        head.direction = "up"
 
+def godown():
+    if head.direction != "up":
+        head.direction = "down"
+
+def goleft():
+    if head.direction != "right":
+        head.direction = "left"
+
+def goright():
+    if head.direction != "left":
+        head.direction = "right"
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+    if head.direction == "left":
+        y = head.ycor()
+        head.sety(x - 20)
+    if head.direction == "right":
+        y = head.ycor()
+        head.sety(x + 20)
+
+wn.listen()
+wn.onkeypress(group, "w")
+wn.onkeypress(godown, "s")
+wn.onkeypress(goleft, "a")
+wn.onkeypress(goright, "d")
+
+#
