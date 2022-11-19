c61b0130ae66c3f97b03385df9f586b2d5b727f3
[JomariMiguel-Carlos] <[jomarimadla0@gmail.com]>
2022-11-19 17:16:01 +0800

    polishing and adding some important features

--- mysnakegame.py
+++ mysnakegame.py
@@ -10,7 +10,7 @@ high_score = 0
 #create the display of the game
 wn = turtle.Screen()
 wn.title("SNAKE GAME")
-wn.bgcolor("yellow")
+wn.bgcolor("black")
 
 #set height and width
 wn.setup(width=600, height=600)
@@ -18,11 +18,11 @@ wn.tracer(0)
 
 #head of the snake
 head = turtle.Turtle()
-head.shape("square")
+head.shape("circle")
 head.color("white")
 head.penup()
 head.goto(0, 0)
-head.direction = "Stop"
+head.direction = "stop"
 
 #food in the game
 food = turtle.Turtle()
@@ -71,11 +71,11 @@ def move():
         y = head.ycor()
         head.sety(y - 20)
     if head.direction == "left":
-        y = head.ycor()
-        head.sety(x - 20)
+        x = head.xcor()
+        head.setx(x - 20)
     if head.direction == "right":
-        y = head.ycor()
-        head.sety(x + 20)
+        x = head.xcor()
+        head.setx(x + 20)
 
 wn.listen()
 wn.onkeypress(group, "w")
@@ -83,4 +83,70 @@ wn.onkeypress(godown, "s")
 wn.onkeypress(goleft, "a")
 wn.onkeypress(goright, "d")
 
-#
+#how the game works
+
+segments = []
+
+while True:
+    wn.update()
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
+        time.sleep(1)
+        head.goto(0, 0)
+        head.direction = "stop"
+        colors = random.choice(['blue', 'red', 'green'])
+        shapes = random.choice(['triangle', 'square', 'circle'])
+        for segment in segments:
+            segment.goto(1000, 1000)
+        segment.clear()
+        score = 0
+        delay = 0.1
+        pen.clear()
+        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Times New Roman", 24, "bold"))
+
+    if head.distance(food) < 20:
+        x = random.randint(-270, 270)
+        y = random.randint(-270, 270)
+        food.goto(x,y)
+
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("circle")
+        new_segment.color("blue") #tail color
+        new_segment.penup()
+        segments.append(new_segment)
+        delay -= 0.001
+        score += 10
+        if score > high_score:
+            high_score = score
+        pen.clear()
+        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center",font=("Times New Roman", 24, "bold"))
+
+    for index in range(len(segments) - 1, 0, -1):
+        x = segments[index - 1].xcor()
+        y = segments[index - 1].ycor()
+        segments[index].goto(x,y)
+
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x,y)
+    move()
+
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0, 0)
+            head.direction = "stop"
+            colors = random.choice(['blue', 'red', 'green'])
+            shapes = random.choice(['triangle', 'square', 'circle'])
+            for segment in segments:
+                segment.goto(1000, 1000)
+            segment.clear()
+
+            score = 0
+            delay = 0.1
+            pen.clear()
+            pen.write("Score: {}  High Score: {}".format(score, high_score), align="center",font=("Times New Roman", 24, "bold"))
+    time.sleep(delay)
+
+wn.mainloop()
